118131694fbf7679dfae220df2d1717a1c5994f7
Husni07 <husnimubarrok07@gmail.com>
2024-05-01 10:57:14 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Comic':'Comic Neue'
+      },
+      colors:{
+        'ungu':'#fb16ba',
+        'merah':'#e80505'
+      }
+    },
+  },
+  darkMode: 'class',
+  plugins: [],
+};
